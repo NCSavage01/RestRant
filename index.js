ca50a148229615ce19b8324cf1403c5bca5536fe
NCSavage01 <cortavias.hatcher@yahoo.com>
2023-08-29 14:20:54 -0400

    Adding places controller

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use('/places', require('./controller/places'))
 app.get('/', function(req, res){
     res.send('Hello world!')
 })
