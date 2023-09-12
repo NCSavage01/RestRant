a50f0d10c74e2d83a1b3d207125a11ef49a4dd67
NCSavage01 <cortavias.hatcher@yahoo.com>
2023-09-11 21:36:03 -0400

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine)
+app.use(express.static('public'))
 app.use('/places', require('./controller/places'))
 app.get('/', function(req, res){
     res.render('Home')
