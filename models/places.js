449b4c38c01627a7478be381f1957e6d1ffc9776
NCSavage01 <cortavias.hatcher@yahoo.com>
2023-09-11 23:44:38 -0400

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Fullers',
+    city: 'Pembroke',
+    state: 'NC',
+    cuisines: 'Southern',
+    places: '/images/Fullers.jpg'
+  }, {
+    name: 'Miyabi',
+    city: 'Fayetteville',
+    state: 'NC',
+    cuisines: 'Hibachi',
+    pic: '/images/miyabi.jpg'
+  }]
