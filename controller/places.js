801091b725cb358c65e79dd294b836555881d555
NCSavage01 <cortavias.hatcher@yahoo.com>
2023-09-12 22:15:32 -0400

    Added the edit form

--- controller/places.js
+++ controller/places.js
@@ -14,24 +14,22 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
   })
-  router.get('/:id', (req, res) => {
-    res.render('places/show')
-    router.get('/:id', (req, res) => {
-        let id = Number(req.params.id)
-        if (isNaN(id)) {
-          res.render('error404')
-        }
-        else if (!places[id]) {
-          res.render('error404')
-        }
-        else {
-          res.render('places/show')
-        }
+  router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id] })
+    }
+  })
+  
         res.render('places/show', { place: places[id] })
 
-      })
       
-  })
   
   
 router.get('/new', (req, res) => {
